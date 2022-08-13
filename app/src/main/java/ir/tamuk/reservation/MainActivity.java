a6367627083a2219cacf66c9000769be40bb0af2
Ghazal <jmzd.developer94@gmail.com>
2022-08-13 10:58:20 +0430

    Initial commit

---
+++ app/src/main/java/ir/tamuk/reservation/MainActivity.java
@@ -0,0 +1,14 @@
+package ir.tamuk.reservation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
